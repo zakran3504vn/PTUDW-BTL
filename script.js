2c461b2214c6dd0f0171fae25b388d0fab3ac819
zakran3504vn <bokunote69@gmail.com>
2023-09-11 06:00:35 +0700

    init project

---
+++ script.js
@@ -0,0 +1,12 @@
+// slider
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+  });
